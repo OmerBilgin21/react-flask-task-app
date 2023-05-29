705f5258a548eff472f785f3345c6b4ee903ec8a
OmerBilgin21 <omrfrkblgn21@gmail.com>
2023-05-29 12:42:52 +0200

    feat: give correct host for mongo and new formatting and make flask auto run the app

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from pymongo import MongoClient
 from uuid import uuid4
 
-client = MongoClient('localhost', 27017)
+client = MongoClient("my-docker-mongo", 27017)
 db = client.flask_db
 todosdb = db.todos
 
@@ -13,27 +13,35 @@ def can_user_do_this(pitch, time):
     user_can_add_event = True
 
     for docs in todosdb.find():
-        if docs['pitch'] == pitch:
-            if docs['time'] == time:
+        if docs["pitch"] == pitch:
+            if docs["time"] == time:
                 user_can_add_event = False
 
     return user_can_add_event
 
 
-@app.post('/save_data')
+@app.post("/save_data")
 def save_event_data():
     if request.form is not None:
         user_can_add_event = can_user_do_this(
-            request.form["pitch"], request.form["time"])
+            request.form["pitch"], request.form["time"]
+        )
 
-        event = request.form['event']
-        person = request.form['person']
-        pitch = request.form['pitch']
-        time = request.form['time']
+        event = request.form["event"]
+        person = request.form["person"]
+        pitch = request.form["pitch"]
+        time = request.form["time"]
 
         if user_can_add_event == True:
             todosdb.insert_one(
-                {"event": event, "person": person, "pitch": pitch, "time": time, "id": str(uuid4())})
+                {
+                    "event": event,
+                    "person": person,
+                    "pitch": pitch,
+                    "time": time,
+                    "id": str(uuid4()),
+                }
+            )
 
             return {"message": "successful"}
 
@@ -42,12 +50,11 @@ def save_event_data():
     return {"message": "failed"}
 
 
-@app.get('/get_event_data')
+@app.get("/get_event_data")
 def get_event_data():
-    current_data = {"event": [], "person": [],
-                    "pitch": [], "time": [], 'id': []}
+    current_data = {"event": [], "person": [], "pitch": [], "time": [], "id": []}
     for x in db.todos.find():
-        current_data["id"].append(str(x['id']))
+        current_data["id"].append(str(x["id"]))
         current_data["event"].append(x["event"])
         current_data["time"].append(x["time"])
         current_data["person"].append(x["person"])
@@ -56,15 +63,15 @@ def get_event_data():
     return current_data
 
 
-@app.put('/edit_data')
+@app.put("/edit_data")
 def edit_data():
     if request.json is not None:
         user_can_edit_event = can_user_do_this(
-            request.json['pitch'], request.json['time'])
+            request.json["pitch"], request.json["time"]
+        )
 
         if user_can_edit_event == True:
-            db.todos.find_one_and_replace(
-                {'id': request.json['id']}, request.json)
+            db.todos.find_one_and_replace({"id": request.json["id"]}, request.json)
 
             return {"message": "successful"}
 
@@ -73,11 +80,15 @@ def edit_data():
     return {"message": "failed"}
 
 
-@app.post('/delete_data')
+@app.post("/delete_data")
 def delete_data():
     if request.json is not None:
-        db.todos.find_one_and_delete({"id": request.json['id']})
+        db.todos.find_one_and_delete({"id": request.json["id"]})
 
         return {"messsage": "successful"}
 
     return {"message": "failed"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
