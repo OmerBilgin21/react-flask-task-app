96333edfbef6de6e96e43949c5362627c862e325
OmerBilgin21 <omrfrkblgn21@gmail.com>
2023-05-29 12:30:56 +0200

    feat(docker): conteinerize the backend and redirect frontend accordingly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     hot: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "http://127.0.0.1:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
